55714581f64c1daaa6c183f9b057dd7fb1f2a7ef
Keshore Durairaj <keshore12@gmail.com>
2025-05-03 01:20:38 +0530

    added chakra ui

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google";
+// // ...
+// // providers: [
+// //   GoogleProvider({
+// //     clientId: process.env.GOOGLE_CLIENT_ID,
+// //     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+// //   })
+// // ]
+// // ...
+
+// const handler = NextAuth({
+// //   ...
+// providers: [
+//       GoogleProvider({
+//     clientId: (process.env.GOOGLE_CLIENT_ID) as string,
+//     clientSecret: (process.env.GOOGLE_CLIENT_SECRET) as string,
+//   })
+// ]
+// })
+
+// export { handler as GET, handler as POST }
